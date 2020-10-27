0933147b7bbaadf5855300873693a686e28ac0a0
elias <eliasfaculdade@yahoo.com.br>
2020-10-27 15:43:34 -0300

    teste2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,64 @@
-alert('meu  primeiro');
+// let nome = 'elias martins';
+// let sobreNome = 'reis';
+
+// //alert(`meu  primeiro script  por : ${nome}`);
+// //console.log(nome);
+// //console.log(nome + ' ' + sobreNome);
+// console.log(nome.replace('elias martins', 'helio martins'));
+
+// let lista = ['carro', 'moto', 'bicicleta'];
+// console.log(lista);
+
+// console.log(lista[0]);
+
+// lista.push('bike2020'); //colcar elemento
+// console.log(lista);
+
+// lista.pop(); //tira ultimo elemento
+// console.log(lista);
+// console.log(lista.length); //tamanho da lista
+
+// console.log(lista.reverse()); //lista inversa
+
+// console.log(lista.toString()); // traz em string
+
+// console.log(lista.join('- ;')); //adicionei -; entre as strings
+
+// let fruta = { nome: 'maça', coe: 'vermelha' };
+// console.log(fruta.nome);
+
+// let frutas = [
+//   { nome: 'maça', coe: 'vermelha' },
+//   { nome: 'uva', coe: 'roxa' },
+// ];
+// console.log(frutas);
+// console.log(frutas[1].nome);
+
+//laços
+
+//let idade = 18;
+// let idade = prompt('Qual sua idade?'); //usario pode digitar na tela (tipo um alert digitavel) exibe uma pergunta e atribui a variavel
+
+// if (idade >= 18) alert('maior de idade' + idade);
+// else alert('menor de idade' + idade);
+
+//for while
+
+//let count = 3;
+// while (count < 5) {
+//   console.log(count);
+//   count++;
+// }
+
+// let count;
+// for (count = 0; count < 5; count++) {
+//   console.log(count);
+// }
+
+// let num = 6;
+// let factorial = 1;
+// for (let i = num; i >= 1; i--) {
+//   factorial *= i;
+//   console.log(factorial);
+// }
+// console.log(factorial);
